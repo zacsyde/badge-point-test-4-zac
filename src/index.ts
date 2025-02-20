7b81eadf9e69b381b50e191847aadaea7bb0eabb
Zuhair Almhithawi <z.almhithawi@dienstec.com>
2025-02-20 09:52:33 +0100

    feat: make getUsersBadge() an asynchronous function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { User } from './types/user.interface';
 import { Icon } from './types/icon.enum';
 
-// Comment for the examinter:
+// Comment for the examiner:
 // We can retrieve these values from a central config source, but for the sake of simplicity I used the constants here
 const BRONZE_THRESHOLD = 5;
 const SILVER_THRESHOLD = 25;
 const GOLD_THRESHOLD = 50;
 
-export const getUsersBadge = (user: User): Icon | null => {
+export const getUsersBadge = async (user: User): Promise<Icon | null> => {
   if (user.solutionCount >= GOLD_THRESHOLD) return Icon.BADGE_GOLD;
   if (user.solutionCount >= SILVER_THRESHOLD) return Icon.BADGE_SILVER;
   if (user.solutionCount >= BRONZE_THRESHOLD) return Icon.BADGE_BRONZE;
